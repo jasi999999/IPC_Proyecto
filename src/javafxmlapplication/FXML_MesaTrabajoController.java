80910f8c4674ab4698f3f53a37371bc7a6db76ee
jasi999999 <152742034+jasi999999@users.noreply.github.com>
2025-05-15 21:05:15 +0200

    Creación de archivos FXML y FXML_Controller de: - Modificar el perfil usuario, casi el mismo que el de crearlo, a 	excepción de la casilla de nombra de usuario que ahora no es 	editable. Se podria reutilizar el otro menú. Puesto que las 	variables son las mismas. - Menu de usuario con las acciones a realizar. - Menu donde realizar recibir los problemas y realizarlos. - Menu de estadísticas de usuario. - Mesa de Trabajo para la Carta Náutica y todo el rollo

---
+++ src/javafxmlapplication/FXML_MesaTrabajoController.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Pablo
+ */
+public class FXML_MesaTrabajoController implements Initializable {
+
+    @FXML
+    private BorderPane rootPane;
+    @FXML
+    private TextField usernameRegistro;
+    @FXML
+    private PasswordField passwordFieldRegistro;
+    @FXML
+    private Button registrarUsuario;
+    @FXML
+    private Button cancelarButton;
+    @FXML
+    private TextField correoElectronicoRegistro;
+    @FXML
+    private DatePicker fechaNacimiento;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleResgistrarUsuario(ActionEvent event) {
+    }
+
+    @FXML
+    private void volverMenuInicial(ActionEvent event) {
+    }
+    
+}
