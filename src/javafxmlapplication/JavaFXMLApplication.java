960b528156fb3d9901e4d9ef7658fd6ace17273d
jasi999999 <152742034+jasi999999@users.noreply.github.com>
2025-05-15 18:34:27 +0200

    Ajustes tamaño de ventana

--- src/javafxmlapplication/JavaFXMLApplication.java
+++ src/javafxmlapplication/JavaFXMLApplication.java
@@ -29,6 +29,8 @@ public class JavaFXMLApplication extends Application {
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
         stage.setTitle("start PROJECT - IPC:");
+        stage.setMinWidth(650);
+        stage.setMinHeight(450);
         stage.show();
     }
 
