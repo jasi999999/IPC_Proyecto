317893ae0d62bef8bf81ab51a4194382cc646dd2
jasi999999 <152742034+jasi999999@users.noreply.github.com>
2025-05-16 11:31:18 +0200

    Crear "startAccion" para poder llamar a todas las interfaces FXML. Hasta que no estén acabadas no se puede implementar más.

--- src/javafxmlapplication/JavaFXMLApplication.java
+++ src/javafxmlapplication/JavaFXMLApplication.java
@@ -40,6 +40,81 @@ public class JavaFXMLApplication extends Application {
         ventanaPrincipal.show();
     }
     
+    public void startMenuEstadisticas(Stage stage) throws Exception {
+        FXMLLoader loaderMenuEstadisticas = new  FXMLLoader(getClass().getResource("FXML_MenuEstadisticas.fxml"));
+        Parent root3 = loaderMenuEstadisticas.load();
+        
+        FXML_RegistrarUsuarioController controller = loaderMenuEstadisticas.getController();
+        controller.setMainApp(this);
+        
+        Scene sceneMenuEstadisticas = new Scene(root3);
+        ventanaPrincipal.setScene(sceneMenuEstadisticas);
+        ventanaPrincipal.setTitle("Estadísticas Usuario");
+        ventanaPrincipal.setMinWidth(650);
+        ventanaPrincipal.setMinHeight(450);
+        ventanaPrincipal.show();
+    }
+    
+    public void startMenuProblema(Stage stage) throws Exception {
+        FXMLLoader loaderMenuProblema = new  FXMLLoader(getClass().getResource("FXML_MenuProblema.fxml"));
+        Parent root5 = loaderMenuProblema.load();
+        
+        FXML_MenuProblemaController controller = loaderMenuProblema.getController();
+        // controller.setMainApp(this);
+        
+        Scene sceneMenuProblema = new Scene(root5);
+        ventanaPrincipal.setScene(sceneMenuProblema);
+        ventanaPrincipal.setTitle("Menú Problema");
+        ventanaPrincipal.setMinWidth(650);
+        ventanaPrincipal.setMinHeight(450);
+        ventanaPrincipal.show();
+    }
+    
+    public void startMenuUsuario(Stage stage) throws Exception {
+        FXMLLoader loaderMenuUsuario = new  FXMLLoader(getClass().getResource("FXML_MenuUsuario.fxml"));
+        Parent root5 = loaderMenuUsuario.load();
+        
+        FXML_MenuUsuarioController controller = loaderMenuUsuario.getController();
+        // controller.setMainApp(this);
+        
+        Scene sceneMenuUsuario = new Scene(root5);
+        ventanaPrincipal.setScene(sceneMenuUsuario);
+        ventanaPrincipal.setTitle("Menú Usuario");
+        ventanaPrincipal.setMinWidth(650);
+        ventanaPrincipal.setMinHeight(450);
+        ventanaPrincipal.show();
+    }
+    
+    public void startMesaTrabajo(Stage stage) throws Exception {
+        FXMLLoader loaderMesaTrabajo = new  FXMLLoader(getClass().getResource("FXML_MesaTrabajo.fxml"));
+        Parent root5 = loaderMesaTrabajo.load();
+        
+        FXML_MesaTrabajoController controller = loaderMesaTrabajo.getController();
+        // controller.setMainApp(this);
+        
+        Scene sceneMesaTrabajo = new Scene(root5);
+        ventanaPrincipal.setScene(sceneMesaTrabajo);
+        ventanaPrincipal.setTitle("Mesa Trabajo");
+        ventanaPrincipal.setMinWidth(650);
+        ventanaPrincipal.setMinHeight(450);
+        ventanaPrincipal.show();
+    }
+    
+    public void startModificarPerfil(Stage stage) throws Exception {
+        FXMLLoader loaderModificarPerfil = new  FXMLLoader(getClass().getResource("FXML_ModificarPerfil.fxml"));
+        Parent root4 = loaderModificarPerfil.load();
+        
+        FXML_ModificarPerfilController controller = loaderModificarPerfil.getController();
+        // controller.setMainApp(this);
+        
+        Scene sceneModificarPerfil = new Scene(root4);
+        ventanaPrincipal.setScene(sceneModificarPerfil);
+        ventanaPrincipal.setTitle("Modificar Perfil");
+        ventanaPrincipal.setMinWidth(650);
+        ventanaPrincipal.setMinHeight(450);
+        ventanaPrincipal.show();
+    }
+    
     public void startRegistrarUsuario() throws Exception {
         // Prueba (se podría lanzar todo esto en un metodo para llamar a estos menus) 
         FXMLLoader loaderRegistrarUsuario = new  FXMLLoader(getClass().getResource("FXML_RegistrarUsuario.fxml"));
@@ -51,21 +126,6 @@ public class JavaFXMLApplication extends Application {
         Scene sceneRegistrarUsuario = new Scene(root2);
         ventanaPrincipal.setScene(sceneRegistrarUsuario);
         ventanaPrincipal.setTitle("Registrar usuario");
-        ventanaPrincipal.setMinWidth(650);
-        ventanaPrincipal.setMinHeight(450);
-        ventanaPrincipal.show();
-    }
-    
-    public void startMenuEstadisticas(Stage stage) throws Exception {
-        FXMLLoader loaderMenuEstadisticas = new  FXMLLoader(getClass().getResource("FXML_MenuEstadisticas.fxml"));
-        Parent root3 = loaderMenuEstadisticas.load();
-        
-        FXML_RegistrarUsuarioController controller = loaderMenuEstadisticas.getController();
-        controller.setMainApp(this);
-        
-        Scene sceneMenuEstadisticas = new Scene(root3);
-        ventanaPrincipal.setScene(sceneMenuEstadisticas);
-        ventanaPrincipal.setTitle("Estadisticas Usuario");
         ventanaPrincipal.setMinWidth(650);
         ventanaPrincipal.setMinHeight(450);
         ventanaPrincipal.show();
