b5575e4adaca0b405b828e358c10740ec298a105
jasi999999 <152742034+jasi999999@users.noreply.github.com>
2025-05-22 10:38:57 +0200

    - Modificar ventanas para que se queden en el tamaño que ha redimensionado el usaurio. - Añadir a la DB la fecha de nacimiento. - Mostrar los errores del campo de registro - Lo mismo en modificarPerfil - Añadir modificarPerfil al DatabaseManage

--- src/javafxmlapplication/UsuarioSimulado.java
+++ src/javafxmlapplication/UsuarioSimulado.java
@@ -4,11 +4,13 @@ public class UsuarioSimulado {
     private final String nick;
     private final String email;
     private final String password;
+    private final String birthDate;
 
-    public UsuarioSimulado(String nick, String email, String password) {
+    public UsuarioSimulado(String nick, String email, String password, String birthDate) {
         this.nick = nick;
         this.email = email;
         this.password = password;
+        this.birthDate = birthDate;
     }
 
     public String getNick() {
@@ -21,5 +23,9 @@ public class UsuarioSimulado {
 
     public String getPassword() {
         return password;
+    }
+    
+    public String getBirthDate() {
+        return birthDate;
     }
 }
